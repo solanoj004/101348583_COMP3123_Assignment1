08415c44a3e1dd2e1323d4290c6875edc7995e98
solanoj004 <95721366+solanoj004@users.noreply.github.com>
2024-10-13 20:46:32 -0400

    employee model finished

---
+++ models/Employee.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  position: { type: String, required: true },
+  salary: { type: Number, required: true },
+  date_of_joining: { type: Date, required: true },
+  department: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Employee', employeeSchema);
